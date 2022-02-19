b2384a52c79f6b588335e39c13db7a1eae0cdad5
arthurisvi <83462514+arthurisvi@users.noreply.github.com>
2022-02-18 22:35:02 -0300

    adding annotation and id strategy

--- src/main/java/com/devsuperior/hrworker/entities/Worker.java
+++ src/main/java/com/devsuperior/hrworker/entities/Worker.java
@@ -3,6 +3,9 @@ package com.devsuperior.hrworker.entities;
 import java.math.BigDecimal;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -16,6 +19,8 @@ import lombok.Setter;
 @Entity
 public class Worker {
     
+    @Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private BigDecimal dailyIncome;
